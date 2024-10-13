f626352d2619529809583241f8f0f106cbeffa17
abeleyasu <abeleyasu98@gmail.com>
2024-10-13 05:18:53 +0000

    task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,36 +1,28 @@
 #!/usr/bin/python3
+"""
+Module to determine if all boxes can be opened.
+"""
+
 def canUnlockAll(boxes):
     """
     Determines if all boxes can be opened.
-    
+
     Args:
-        boxes: A list of lists, where each sublist contains the keys to other boxes.
-    
+        boxes (list): A list of lists where each list represents
+                      the keys in that box.
+
     Returns:
-        True if all boxes can be opened, False otherwise.
+        bool: True if all boxes can be opened, False otherwise.
     """
     n = len(boxes)
-    unlocked = [False] * n  # Keep track of unlocked boxes
-    unlocked[0] = True  # Box 0 is initially unlocked
-    keys = [0]  # Start with the keys from box 0
-    
+    opened = [False] * n  # Track which boxes are opened
+    opened[0] = True  # The first box is already unlocked
+    keys = boxes[0]  # Start with the keys in the first box
+
     while keys:
-        current_box = keys.pop()  # Get the next box to explore
-        
-        for key in boxes[current_box]:
-            if key < n and not unlocked[key]:
-                unlocked[key] = True  # Unlock the box
-                keys.append(key)  # Add the keys from the newly unlocked box
-    
-    # Check if all boxes are unlocked
-    return all(unlocked)
+        key = keys.pop()  # Get the next key
+        if not opened[key]:  # If this box is not opened yet
+            opened[key] = True  # Mark the box as opened
+            keys.extend(boxes[key])  # Add its keys to the keys list
 
-if __name__ == "__main__":
-    boxes = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes))  # True
-
-    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes))  # True
-
-    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes))  # False
+    return all(opened)  # Check if all boxes are opened
