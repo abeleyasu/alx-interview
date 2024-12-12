54aa88c8914af0655fb778d51130623fb3fda892
abeleyasu <abeleyasu98@gmail.com>
2024-12-12 08:39:43 +0000

    0x0A. Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,45 +1,44 @@
+#!/usr/bin/python3
+
 def isWinner(x, nums):
-    """Determines the winner of the prime game.
-    
-    Args:
-        x (int): Number of rounds.
-        nums (list): List of n values for each round.
-    
-    Returns:
-        str or None: Name of the player with the most wins or None if undecided.
-    """
-    if not nums or x < 1:
-        return None
-    
-    # Find the maximum number in nums to limit prime computation
-    max_n = max(nums)
-    
-    # Sieve of Eratosthenes to compute primes up to max_n
-    primes = [True] * (max_n + 1)
-    primes[0] = primes[1] = False  # 0 and 1 are not prime numbers
-    for i in range(2, int(max_n**0.5) + 1):
-        if primes[i]:
-            for j in range(i * i, max_n + 1, i):
-                primes[j] = False
+    # Function to generate all prime numbers up to n using Sieve of Eratosthenes
+    def sieve_of_eratosthenes(n):
+        primes = [True] * (n + 1)
+        primes[0] = primes[1] = False  # 0 and 1 are not primes
+        for i in range(2, int(n**0.5) + 1):
+            if primes[i]:
+                for j in range(i * i, n + 1, i):
+                    primes[j] = False
+        return [i for i in range(2, n + 1) if primes[i]]
 
-    # Precompute the number of primes less than or equal to each number
-    prime_count = [0] * (max_n + 1)
-    for i in range(1, max_n + 1):
-        prime_count[i] = prime_count[i - 1] + (1 if primes[i] else 0)
+    # Function to determine the winner for a single round
+    def play_game(n):
+        primes = sieve_of_eratosthenes(n)
+        turn = 0  # 0 for Maria, 1 for Ben
+        # Simulate the game
+        while primes:
+            # Maria's or Ben's turn
+            current_prime = primes.pop(0)
+            # Remove multiples of the current prime
+            primes = [p for p in primes if p % current_prime != 0]
+            turn = 1 - turn  # Switch turn
+        # If turn is 0, Maria was last to play, meaning Ben won, otherwise Maria won
+        return 1 if turn == 0 else 0
 
-    # Determine the winner for each round
     maria_wins = 0
     ben_wins = 0
-    for n in nums:
-        if prime_count[n] % 2 == 0:  # Ben wins if the count of primes is even
-            ben_wins += 1
-        else:  # Maria wins if the count of primes is odd
-            maria_wins += 1
 
-    # Return the overall winner
+    for n in nums:
+        if play_game(n) == 0:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    # Return the player with the most wins
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
         return "Ben"
     else:
         return None
+
